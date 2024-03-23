21697b6ed739fad2509838b69351e221386bbb96
MinIcecream <63301542+MinIcecream@users.noreply.github.com>
2024-03-23 11:07:28 -0400

    connecting frontend and backend

--- Lesson 2/phonebook/src/services/api.js
+++ Lesson 2/phonebook/src/services/api.js
@@ -1,7 +1,7 @@
  
 import axios from 'axios'
 
-const baseUrl = "http://localhost:3001/persons" 
+const baseUrl = "/api/notes" 
 
 const create = newPerson => {
     return axios.post(baseUrl, newPerson)
