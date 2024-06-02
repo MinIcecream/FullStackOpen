21df60baf10f9537f77d17b28c4a35da927baa95
MinIcecream <63301542+MinIcecream@users.noreply.github.com>
2024-06-01 21:52:02 -0400

    Finished 7b

--- Lesson7/routed-anecdotes/src/App.jsx
+++ Lesson7/routed-anecdotes/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import {Link, Route, Routes, useMatch, useNavigate} from "react-router-dom"
 import { Anecdote } from './Anecdote'
+import { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -46,20 +47,27 @@ const Footer = () => (
   </div>
 )
 
-const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+const CreateNew = (props) => { 
+  const content = useField('content')
+  const author = useField('author')
+  const info = useField('info')
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.inputProps.value,
+      author: author.inputProps.value,
+      info: info.inputProps.value,
       votes: 0
     })
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
   }
 
   return (
@@ -68,17 +76,24 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input 
+            {...content.inputProps}
+          />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input 
+            {...author.inputProps}
+          />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input 
+            {...info.inputProps}
+          />
         </div>
         <button>create</button>
+        <button onClick = {handleReset}>reset</button>
       </form>
     </div>
   )
